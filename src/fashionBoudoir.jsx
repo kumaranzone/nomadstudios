36c15b9380513353917fc21fe6132897e2147f2b
Kumaran <mmurugesan@tekion.com>
2017-11-17 07:58:11 +0530

    Sending images for slider as props

--- src/fashionBoudoir.jsx
+++ src/fashionBoudoir.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import MultiSlider from './reactSlider';
 import VideoPlayer from './videoSection';
 
+const ImagesForSlider1 = [
+  'https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-2-1.jpg',
+  'https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-2-2.jpg',
+  'https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-2-3.jpg',
+]
+
 export default () => {
   return(
     <div className="col-md-12">
-      <MultiSlider />
+      <MultiSlider sliderImages1={ImagesForSlider1} />
       <VideoPlayer /> 
     </div>
   )
--- src/reactSlider.jsx
+++ src/reactSlider.jsx
@@ -16,12 +16,8 @@ class MultiSlider extends React.Component {
     )
   }
   render(){
-
-    const ImagesForSlider1 = [
-      'https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-2-1.jpg',
-      'https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-2-2.jpg',
-      'https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-2-3.jpg',
-    ]
+    const {sliderImages1} = this.props;
+   
     const settings1 = {
       //dots: true,
       //speed: 500,
@@ -40,7 +36,10 @@ class MultiSlider extends React.Component {
     };
     return (
       <div className="slider-container">
-       {/*  <Slider {...settings1}>
+        <Slider {...settings2}>
+          { this.SliderItems(sliderImages1) }
+        </Slider>
+        {/*  <Slider {...settings1}>
           <div className="col-xs-12 col-sm-6 col-md-3">
             <a href="#"><img src="https://s3-us-west-1.amazonaws.com/www.nomadstudios.in/img/menu/model-1-1.jpg" alt="slide-1-img-1" className="img-responsive" /></a>
           </div>
@@ -57,9 +56,6 @@ class MultiSlider extends React.Component {
             <a href="#"><img src="http://placehold.it/300/002d5a/fff/&text=6" className="img-responsive" /></a>
           </div> 
         </Slider>*/}
-        <Slider {...settings2}>
-          { this.SliderItems(ImagesForSlider1) }
-        </Slider>
       </div>
     );
   }
